8c122f88b630892f5317e11cfc3339e6c2f9a4b5
Rome Glez <rome_gs@hotmail.com>
2019-08-18 23:47:52 -0500

    Clases/Objetos

--- Clases y Objetos/Source.cpp
+++ Clases y Objetos/Source.cpp
@@ -15,10 +15,25 @@ public://Metodos
 	void correr();
 };
 
+class Rectangulo {
+private:
+	int largo;
+	int ancho;
+public:
+	Rectangulo(int, int);
+	void perimetro();
+	void area();
+};
+
 //Constructor, srive para inicializar los atributos
 Persona::Persona(int _edad, string _nombre) {
 	edad = _edad;
 	nombre = _nombre;
+}
+
+Rectangulo:: Rectangulo (int _largo, int _ancho) {
+	largo = _largo;
+	ancho = _ancho;
 }
 
 void Persona::leer() {
@@ -29,6 +44,20 @@ void Persona::correr() {
 	cout << nombre << " is running" << endl;
 }
 
+void Rectangulo::perimetro() {
+
+	int p;
+	p = largo * 2 + ancho * 2;
+	cout << "El perimetro es: " << p << endl;
+}
+
+void Rectangulo::area() {
+
+	int a;
+	a = largo * ancho;
+	cout << "El area es: " << a << endl;
+}
+
 int main() {
 
 	Persona p1 = Persona(20, "Rome");
@@ -36,6 +65,11 @@ int main() {
 
 	p1.leer();
 	p2.correr();
+
+	Rectangulo r1 = Rectangulo(5, 7);
+
+	r1.area();
+	r1.perimetro();
 
 	system("pause");
 	return 0;
